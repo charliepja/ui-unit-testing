41edeb1675fbd213df5a581523d9637fe3352884
Charlie Anderson <charliepjanderson@gmail.com>
2020-02-21 17:37:01 +0000

    tests that dividing by 0 means error, works

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -78,9 +78,15 @@ describe('calculator functionality', function() {
 		element(by.css('#operator_equals')).click();
 		expect(running_total.getAttribute('value')).to.eventually.equal('10000000')
 	})
-	//
-	// it('should result in error when dividing by 0', function() {
-	//
-	// })
+
+	it('should result in error when dividing by 0', function() {
+		running_total = element(by.css('#running_total'))
+		element(by.css('#number1')).click();
+		element(by.css('#number0')).click();
+		element(by.css('#operator_divide')).click();
+		element(by.css('#number0')).click();
+		element(by.css('#operator_equals')).click();
+		expect(running_total.getAttribute('value')).to.eventually.equal('Error')
+	})
 
 });
