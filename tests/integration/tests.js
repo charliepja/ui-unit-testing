0cdfeaaf50af69f8849281f430ac24bc3f6936ac
Charlie Anderson <charliepjanderson@gmail.com>
2020-02-21 17:15:38 +0000

    tests chain multiple operations together, works

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,4 +16,48 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+	it('should be able to display the result of an operation', function() {
+		running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('30')
+	})
+
+	it('should be able to chain multiple operations together', function() {
+		running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_subtract')).click();
+		element(by.css('#number5')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('25')
+	})
+	//
+	// it('should work with positive numbers', function() {
+	//
+	// })
+	//
+	// it('should work with negative numbers', function() {
+	//
+	// })
+	//
+	// it('should work with decimal numbers', function() {
+	//
+	// })
+	//
+	// it('should work with large numbers', function() {
+	//
+	// })
+	//
+	// it('should result in error when dividing by 0', function() {
+	//
+	// })
+
 });
